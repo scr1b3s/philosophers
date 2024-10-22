b7f826f58e48c6ad8f7425c46cc0411100eeae98
André Ferreira <andrde-s@student.42.rio>
2024-10-22 00:11:12 -0300

    Fixing the issues on INT_MAX

--- philo/parse_input.c
+++ philo/parse_input.c
@@ -14,7 +14,7 @@
 
 /* Parse Input
  * @brief Parses the input arguments and stores them in the table.
- * 
+ *
  * @param table A pointer to the table structure.
  * @param av The input arguments.
  *                            av[1] av[2] av[3] av[4]  av[5]
@@ -29,41 +29,42 @@
  *         - If the number is over 10 digits, it's over INT_MAX.
  * 3. The Timestamps must be over 60ms.
  */
-static long ft_atol(const char *str);
-static size_t ft_strlen(const char *str);
+static long		ft_atol(const char *str);
+static size_t	ft_strlen(const char *str);
 
-static long ft_atol(const char *str)
+static long	ft_atol(const char *str)
 {
-    long    res;
-    int     sign;
+	long	res;
+	int		sign;
 
-    res = 0;
-    sign = 1;
-    if (ft_strlen(str) > 10)
-        return (-1);
-    while (*str == ' ' || (*str >= 9 && *str <= 13))
-        str++;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            return (-1);
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        res = res * 10 + *str - '0';
-        str++;
-    }
-    return (res * sign);
+	res = 0;
+	sign = 1;
+	if (ft_strlen(str) > 10 || str == "2147483647")
+		return (-1);
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			return (-1);
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	if (res == 2147483647)
+		return (-1);
+	return (res * sign);
 }
 
-static size_t ft_strlen(const char *str)
+static size_t	ft_strlen(const char *str)
 {
-    size_t  i;
+	size_t i;
 
-    i = -1;
-    while (str[++i])
-        continue ;
-    
-    return (i);
+	i = -1;
+	while (str[++i])
+		continue ;
+	return (i);
 }
