bf235a40c46224f52a29e5f2d65bed0b30d90fbb
Kobamaru <scribearm@gmail.com>
2024-10-23 01:04:43 -0300

    Testing.

--- philo/tests/test_parse_input.c
+++ philo/tests/test_parse_input.c
@@ -17,8 +17,8 @@ Test(parse_input_suite, test_fewer_than_5_arguments, .exit_code = 1) {
     char *args[] = {"program", "5", "200", "200", "200"};
     int argc = 4;
 
-    cr_log_info("Testing with fewer than 5 arguments");
-    cr_assert_eq(parse_input(&table, argc, args), "");
+    cr_log_info("Testing with fewer than 5 arguments, should return 1.");
+    cr_assert_eq(parse_input(&table, argc, args));
 }
 
 Test(parse_input_suite, test_more_than_6_arguments, .exit_code = 1) {
@@ -26,8 +26,8 @@ Test(parse_input_suite, test_more_than_6_arguments, .exit_code = 1) {
     char *args[] = {"program", "5", "200", "200", "200", "5", "extra"};
     int argc = 7;
 
-    cr_log_info("Testing with more than 6 arguments");
-    cr_assert_eq(parse_input(&table, argc, args), "");
+    cr_log_info("Testing with more than 6 arguments, should return 1.");
+    cr_assert_eq(parse_input(&table, argc, args));
 }
 
 Test(parse_input_suite, test_valid_input) {
@@ -49,8 +49,8 @@ Test(parse_input_suite, test_negative_philosophers, .exit_code = 1) {
     char *args[] = {"program", "-5", "200", "200", "200"};
     int argc = 5;
 
-    cr_log_info("Testing with negative number of philosophers");
-    cr_assert_eq(parse_input(&table, argc, args), "");
+    cr_log_info("Testing with negative number of philosophers, should return 1.");
+    cr_assert_eq(parse_input(&table, argc, args));
 }
 
 Test(parse_input_suite, test_timestamps_less_than_60ms, .exit_code = 1) {
@@ -58,8 +58,8 @@ Test(parse_input_suite, test_timestamps_less_than_60ms, .exit_code = 1) {
     char *args[] = {"program", "5", "50", "50", "50"};
     int argc = 5;
 
-    cr_log_info("Testing with timestamps less than 60ms");
-    cr_assert_eq(parse_input(&table, argc, args), "");
+    cr_log_info("Testing with timestamps less than 60ms, should return 1.");
+    cr_assert_eq(parse_input(&table, argc, args));
 }
 
 Test(parse_input_suite, test_philosophers_over_INT_MAX, .exit_code = 1) {
@@ -67,8 +67,8 @@ Test(parse_input_suite, test_philosophers_over_INT_MAX, .exit_code = 1) {
     char *args[] = {"program", "2147483648", "200", "200", "200"};
     int argc = 5;
 
-    cr_log_info("Testing with number of philosophers over INT_MAX");
-    cr_assert_eq(parse_input(&table, argc, args), "");
+    cr_log_info("Testing with number of philosophers over INT_MAX, should return 1.");
+    cr_assert_eq(parse_input(&table, argc, args));
 }
 
 Test(parse_input_suite, test_valid_input_without_optional_meals) {
