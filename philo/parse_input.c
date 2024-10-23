b2a343d791cf91c5ecd7f269c97a08866a50fc3b
Kobamaru <scribearm@gmail.com>
2024-10-22 21:44:12 -0300

    Made some changes, specifically checking for bad exit status and such.

--- philo/parse_input.c
+++ philo/parse_input.c
@@ -42,10 +42,17 @@ int	parse_input(t_table *table, int ac, char **av)
 	table->time_to_die = ft_atol(av[2]) * 1e3;
 	table->time_to_eat = ft_atol(av[3]) * 1e3;
 	table->time_to_sleep = ft_atol(av[4]) * 1e3;
+	
 	if (table->time_to_die < 6e4
 		|| table->time_to_eat < 6e4
 		|| table->time_to_sleep < 6e4)
 		return (-1);
+
+	if (table->time_to_die == -1
+		|| table->time_to_eat == -1
+		|| table->time_to_sleep == -1)
+		return (-1);
+
 	if (av[5])
 		table->num_meals = ft_atol(av[5]);
 	else
